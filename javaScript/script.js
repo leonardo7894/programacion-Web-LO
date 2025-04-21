e92c8685819c87d71c4f48ccd334ca2798f072b0
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-04-21 17:02:54 -0300

    primer clase con java script

---
+++ javaScript/script.js
@@ -0,0 +1,79 @@
+//lenguaje de programacion 
+
+var mivariable = 'hola'// no se usa
+let mivariable2 = 'varable con let'//solo vive en el scope 
+const miveriable3 = "constante no cambia los valores"
+
+function test(){ //para hacer una funcion
+    let mivariable4 = ''
+}
+
+const hola = 'hola leo';
+console.log(hola);//es el equivalente a un print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.
