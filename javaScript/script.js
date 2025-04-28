192de4ab45dd2b9cd487ce38bd9db524f1f75fb7
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-04-28 16:51:58 -0300

    en java script hicimosalgunos ejercicios iniciales y conocimos algunos comandos

--- javaScript/script.js
+++ javaScript/script.js
@@ -13,67 +13,38 @@ console.log(hola);//es el equivalente a un print
 
 
 
+// bucle for 
+const arrayNumeros = [1,2,3];
+
+for(let i = 0; i<arrayNumeros.length; i++){
+    console.log(arrayNumeros)
+}
 
 
 
+const numero1 = 0.5
+const numero2 = 100
 
+if (numero1 < numero2) {
+   console.log(`el numero ${numero2} es mayor`)
+}
+ 
+else {
+    console.log(`el numero es menor a ${numero1}`)
+}
 
+let numero = 99
+let contador = 0
+while(contador <= numero){
+    contador++
+    console.log(`el contador esta en ${contador}`);
+}
 
+let numero4 = 4
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
+let suma = 0
+while(suma <= numero4 ){
+    
+   suma += suma
+   console.log(`la suma da ${suma}`)
+}
